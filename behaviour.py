fd35ad851e2d78a88fa27b5d309b3801cc79df83
SimpleSavely <42109246+SimpleSavely@users.noreply.github.com>
2019-02-01 23:03:28 +0700

    Update behaviour.py

--- behaviour.py
+++ behaviour.py
@@ -6,31 +6,31 @@ first_message_id = 'familiarity'
 behavior = {
     'familiarity': {
         'text': 'Привет! Я шаблонный бот, созданный пользователями https://vk.com/perfectrum и https://vk.com/simplesavely. Можете посмотреть мои исходники или написать мне что-нибудь в базу данных.',
-        'type': 0,
+        'type': 1,
         'expect': ['Покажи исходники', 'Запиши в БД'],
         'children': ['code', 'db']
         },
     'options': {
         'text': 'Можете посмотреть мои исходники или написать мне что-нибудь в базу данных.',
-        'type': 0,
+        'type': 1,
         'expect': ['Покажи исходники', 'Запиши в БД'],
         'children': ['code', 'db']
         },
     'code': {
-        'text':'Репозиторий с моими исходниками лежит по ссылке: ',
-        'type': 0,
+        'text':'А вот и не покажу',
+        'type': 1,
         'expect':['назад'],
         'children':['options']
         },
     'db':{
         'text':'Что мне записать?',
-        'type': 1,
+        'type': 2,
         'expect':[],
         'children':['got_it']
             },
     'got_it': {
         'text': 'Записал, спасибо!',
-        'type': 0,
+        'type': 1,
         'expect': ['Назад'],
         'children': ['options']
         }
